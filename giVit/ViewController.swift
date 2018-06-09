fa9995ba1137eb3b6ae2b6fa3cdada2a6ae6eb2b
RoyalKingMomo <mohammad@enggmomo.me>
2018-06-09 14:34:33 -0600

    add preliminary xcode project

---
+++ giVit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  giVit
+//
+//  Created by Mohammad Al-Ahdal on 2018-06-09.
+//  Copyright © 2018 EnggMomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
