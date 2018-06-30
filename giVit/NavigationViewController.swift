0f5e760a62d539d3272e9e8a5d5364153fcf4727
Megnath Ramesh <megnath@scopear.com>
2018-06-30 17:41:47 -0600

    Adding a new storyboard for maps

---
+++ giVit/NavigationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NavigationViewController.swift
+//  giVit
+//
+//  Created by Megnath Ramesh on 2018-06-30.
+//  Copyright © 2018 EnggMomo. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class NavigationViewController:UIViewController{
+    
+}
