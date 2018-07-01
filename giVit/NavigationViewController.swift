2890a4c1de27d1eb929547ce0f6aed3791ad13aa
Megnath Ramesh <megnath@ualberta.ca>
2018-06-30 18:37:17 -0600

    Adding location services to google maps view

--- giVit/NavigationViewController.swift
+++ giVit/NavigationViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 import GoogleMaps
 
 class NavigationViewController:UIViewController{
+    
+    
+    
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate
         let camera = GMSCameraPosition.camera(withLatitude: 53.32, longitude: -113.29, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
+        mapView.isMyLocationEnabled = true
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 53.32, longitude: -113.29)
+        marker.position = mapView.myLocation != nil ? mapView.myLocation!.coordinate : CLLocationCoordinate2D(latitude: 53.32, longitude: -113.29)
         marker.title = "Edmonton"
         marker.snippet = "Canada"
         marker.map = mapView
